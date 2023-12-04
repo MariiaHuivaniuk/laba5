553d648a36e610b22242621c4821d5a1edea038d
Masha <huivaniuk.mariia@chnu.edu.ua>
2023-12-04 11:05:37 +0100

    Add project files.

---
+++ laba5/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Завдання 1");
+
+        Console.WriteLine("operand1= ");
+        double operand1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("operand2= ");
+        double operand2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("operation= ");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        double result = PerformingTheOperation(operand1, operand2, operation);
+
+        Console.WriteLine($"Результат: {result}");
+
+    }
+
+    static double PerformingTheOperation(double operand1, double operand2, char operation)
+    {
+        switch (operation)
+        {
+            case '+':
+                return operand1 + operand2;
+            case '-':
+                return operand1 - operand2;
+            case '*':
+                return operand1 * operand2;
+            case '/':
+                return operand1 / operand2;
+
+            default:
+                Console.WriteLine("Не вірна операція");
+                return double.NaN;
+        }
+
+        
+        
+    }
+}
+
+
